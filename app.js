070810243257b501992207b7dff47fb9e8badcf9
TalalNuman <75552390+TalalNuman@users.noreply.github.com>
2021-06-28 10:31:47 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let it = require('ityped');
+
+    window.fun = function fun(){
+        var x =  count; 
+        it.init(x, {disableBackTyping:true, showCursor: false, strings: ['8', '7', '6', '5', '4' ,'3', '2', '1', '0',"Congrats You're dead." ] });
+    }
+
